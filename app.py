5dfe5a29024670e5de996f7ec7794c8474afb071
youngkwonjo <cccpy98@gmail.com>
2019-01-22 13:47:03 +0900

    -

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from flask import Flask
 from flask_cors import CORS
 
 #app = Flask(__name__)
-app = Flask(__name__, static_folder='./static/', template_folder='./static')
+app = Flask(__name__, static_folder='./static')#, template_folder='./static')
 ### http://localhost:5000/static/index.html
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 from model.test import init_db as init_db_test
 init_db_test()
-
 
 
 @app.route("/")
